951f3b3f86ef234e272cbfddf44e43d6e4381cb7
CalebCrystal <144365080+CalebCrystal@users.noreply.github.com>
2023-09-28 10:32:04 -0500

    main.cpp

--- lab2-Cylinder/main.cpp
+++ lab2-Cylinder/main.cpp
@@ -4,6 +4,7 @@
 
 #include<iostream>
 #include"Cylinder.h"
+#include"iomanip"
 
 int main()
 {
@@ -12,13 +13,13 @@ int main()
 	b.setHeight(3.0); // sets Height 
 	b.setRadius(3.0); // sets Radius
 
-	 std::cout << "Height of Cylinder: " << b.getHeight() << std::endl << "Radius of Cylinder: " << b.getRadius() << std::endl;
+	 std::cout << "Height of Cylinder: " << std::setprecision(6) << b.getHeight() << std::endl << "Radius of Cylinder: " << b.getRadius() << std::endl;
 	 // outputs the height and the radius of the cylinder to the screen 
 
-	 std::cout << "Surface Area of the Cylinder: " << b.areaOfCylinder() <<  std::endl;
+	 std::cout << "Surface Area of the Cylinder: " << std::setprecision(6) << b.areaOfCylinder() <<  std::endl;
 	 // calls the area of a Cylinder function to calculate the surface area with the given height and set radius
 
-	 std::cout << "Volume of the Cylinder: " << b.VolOfCylinder() << std::endl;
+	 std::cout << "Volume of the Cylinder: " << std::setprecision(6) << b.VolOfCylinder() << std::endl;
 	 // calls the Voulme of Cylinder funtion this calculates the volume of the cylinder in the class Cylinder 
 	 // file and returns the value to this screen
 
